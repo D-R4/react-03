f106797c06ed57119ed2ba49ffd9f20967a79f06
D-R4 <ibrahimtsuev1@mail.ru>
2022-12-24 12:57:05 +0300

    first commit

---
+++ src/Base.jsx
@@ -0,0 +1,11 @@
+import DR from './public/rc.png'
+function Base(){
+    return(
+        <div>
+            <h1>I'm Declan Rice</h1>
+            <img src={DR} alt="" />
+        </div>
+        
+    )
+}
+export default Base
